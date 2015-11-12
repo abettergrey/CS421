7e60d62eaea26607b7c5123264a92f97b8c150f6
Brett Yeager <abettergrey@gmail.com>
2015-11-12 14:15:15 -0500

    stuff

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("hi World");
+        System.out.println("hi Dr.Cho");
     }
 
 }
